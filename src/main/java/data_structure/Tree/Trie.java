4ea37293b8a7a4e06ad7cb2ff550b27a89d43102
blackcocoon <42939960+blackcocoon@users.noreply.github.com>
2021-04-20 19:39:40 +0900

    주제별 디렉토리 생성

---
+++ src/main/java/data_structure/Tree/Trie.java
@@ -0,0 +1,46 @@
+package data_structure.Tree;
+
+import java.util.HashMap;
+
+public class Trie {
+
+    private final Node root = new Node();
+
+    private static class Node {
+        HashMap<String, Node> children = new HashMap<>();
+        boolean isWord = false;
+    }
+
+    public void insert(String word) {
+        Node cur = root;
+        for (String split : word.split("")) {
+            if (cur.children.containsKey(split)) {
+                cur = cur.children.get(split);
+                continue;
+            }
+            Node nextNode = new Node();
+            cur.children.put(split, nextNode);
+            cur = nextNode;
+        }
+        cur.isWord = true;
+    }
+
+    public boolean search(String word) {
+        Node node = getNode(word);
+        return node != null && node.isWord;
+    }
+
+    private Node getNode(String word) {
+        Node cur = this.root;
+        for (String s : word.split("")) {
+            if (!cur.children.containsKey(s))
+                return null;
+            cur = cur.children.get(s);
+        }
+        return cur;
+    }
+
+    public boolean startWith(String prefix) {
+        return getNode(prefix) != null;
+    }
+}
