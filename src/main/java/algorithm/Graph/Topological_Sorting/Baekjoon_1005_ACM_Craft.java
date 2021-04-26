d45652596b09796b7281aad006b029b3a0c4e262
blackcocoon <42939960+blackcocoon@users.noreply.github.com>
2021-04-26 20:03:40 +0900

    알고리즘 | Topological Sorting | Baekjoon_1005_ACM_Craft 추가

---
+++ src/main/java/algorithm/Graph/Topological_Sorting/Baekjoon_1005_ACM_Craft.java
@@ -0,0 +1,58 @@
+package algorithm.Graph.Topological_Sorting;
+
+import java.util.*;
+
+public class Baekjoon_1005_ACM_Craft {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        for (int i = 0; i < T; i++) {
+            int N = sc.nextInt();
+            int K = sc.nextInt();
+
+            int[] inDegrees = new int[N + 1];
+            int[] result = new int[N + 1];
+
+            int[] buildTimes = new int[N + 1];
+            for (int j = 1; j < N + 1; j++)
+                buildTimes[j] = sc.nextInt();
+
+            ArrayList<ArrayList<Integer>> nodes = new ArrayList<>();
+            for (int j = 0; j <= N; j++)
+                nodes.add(new ArrayList<>());
+
+            for (int j = 0; j < K; j++) {
+                int cur = sc.nextInt();
+                int adjacent = sc.nextInt();
+                nodes.get(cur).add(adjacent);
+                inDegrees[adjacent]++;
+            }
+
+            int target = sc.nextInt();
+
+            Queue<Integer> q = new LinkedList<>();
+            for (int j = 1; j < N + 1; j++) {
+                if (inDegrees[j] == 0) {
+                    result[j] = buildTimes[j];
+                    q.offer(j);
+                }
+            }
+
+            while (!q.isEmpty()) {
+                int cur = q.poll();
+                ArrayList<Integer> adjacents = nodes.get(cur);
+
+                for (int adjacent : adjacents) {
+                    result[adjacent] = Math.max(result[adjacent], result[cur] + buildTimes[adjacent]);
+                    if (--inDegrees[adjacent] == 0)
+                        q.offer(adjacent);
+                }
+            }
+
+            System.out.println(result[target]);
+        }
+    }
+}
