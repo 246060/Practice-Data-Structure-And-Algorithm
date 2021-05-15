5e854322c6c75a8bbc673a98a4b5ea870a46860a
blackcocoon <42939960+blackcocoon@users.noreply.github.com>
2021-05-16 03:00:57 +0900

    알고리즘 | Search | 예제 파일 추가

---
+++ src/main/java/algorithm/Search/UpperBound.java
@@ -0,0 +1,36 @@
+package algorithm.Search;
+
+/**
+ * Upper bound search algorithm.
+ * Upper bound is kind of binary search algorithm but:
+ * -It returns index of first element which is grater than searched value.
+ * -If searched element is bigger than any array element function returns first index after last element.
+ * Behaviour for unsorted arrays is unspecified.
+ * Complexity O(log n).
+ *
+ * @author Bartlomiej Drozd <mail@bartlomiejdrozd.pl>
+ * @author Justin Wetherell <phishman3579@gmail.com>
+ */
+public class UpperBound {
+    // https://www.javatips.net/api/java-algorithms-implementation-master/src/com/jwetherell/algorithms/search/UpperBound.java
+
+    private UpperBound() { }
+
+    public static int upperBound(int[] array, int length, int value) {
+
+        int low = 0;
+        int high = length;
+
+        while (low < high) {
+            final int mid = (low + high) / 2;
+
+            if (value >= array[mid]) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+
+        return low;
+    }
+}
