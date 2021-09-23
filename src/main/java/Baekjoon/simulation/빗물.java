634e9e89783d18c2f795d25bccbd200ac8f850af
blackcocoon <42939960+blackcocoon@users.noreply.github.com>
2021-09-23 22:43:52 +0900

    백준 시뮬레이션 문제 추가

---
+++ src/main/java/Baekjoon/simulation/빗물.java
@@ -0,0 +1,39 @@
+package Baekjoon.simulation;
+
+import java.util.Scanner;
+
+public class 빗물 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int H = sc.nextInt();
+        int W = sc.nextInt();
+
+        int[] hs = new int[W];
+        for (int i = 0; i < W; i++) {
+            hs[i] = sc.nextInt();
+        }
+
+        int answer = 0;
+        int j = 0;
+
+        while (j < W) {
+            int left = Integer.MIN_VALUE;
+            for (int i = j; i >= 0; i--)
+                left = Math.max(left, hs[i]);
+
+            int right = Integer.MIN_VALUE;
+            for (int i = j; i < W; i++)
+                right = Math.max(right, hs[i]);
+
+            if (left > right) {
+                answer += right - hs[j];
+            } else {
+                answer += left - hs[j];
+            }
+            j++;
+        }
+
+        System.out.println(answer);
+    }
+}
