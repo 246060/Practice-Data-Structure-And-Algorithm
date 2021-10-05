1925b0755f76cdadad00bf2f944101d1d5b92ffe
blackcocoon <42939960+blackcocoon@users.noreply.github.com>
2021-10-06 07:44:52 +0900

    문제 풀이 추가

---
+++ src/main/java/Coding_Test/Baekjoon/구현/주차의_신.java
@@ -0,0 +1,25 @@
+package Coding_Test.Baekjoon.구현;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class 주차의_신 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            int n = sc.nextInt();
+
+            int[] stores = new int[n];
+            for (int j = 0; j < n; j++)
+                stores[j] = sc.nextInt();
+
+            Arrays.sort(stores);
+            int dist = (stores[n - 1] - stores[0]) * 2;
+
+            System.out.println(dist);
+        }
+    }
+}
