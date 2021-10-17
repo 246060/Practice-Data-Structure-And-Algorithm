8f62844f1e162aa8f1ff408b9e62353e51416ead
blackcocoon <42939960+blackcocoon@users.noreply.github.com>
2021-10-18 05:39:43 +0900

    프로젝트 구조 변경중

--- src/main/java/concept_study/algorithm/sort/bubble_sort/BubbleSort.java
+++ src/main/java/concept_study/algorithm/sort/bubble_sort/BubbleSort.java
@@ -1,13 +1,29 @@
 package concept_study.algorithm.sort.bubble_sort;
 
-// TODO
+import java.util.Arrays;
+
 public class BubbleSort {
 
     public static void main(String[] args) {
+
         int[] arr = {12, 11, 13, 5, 6, 7};
+        System.out.println(Arrays.toString(arr));
+
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
     }
 
     static void sort(int[] arr) {
+        int n = arr.length;
 
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < n - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                }
+            }
+        }
     }
 }
